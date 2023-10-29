1b53003115bddbf222be624ea201992640f6cbd3
Sena Hazir <senahazir@berkeley.edu>
2023-10-29 03:28:16 -0400

    form updates

--- src/containers/FormPage.js
+++ src/containers/FormPage.js
@@ -9,7 +9,6 @@ import { ProviderForm } from "../components/ProviderForm";
 export function FormPage() {
   return (
     <div className="h-full">
-      <h1>Form Page</h1>
       <ProviderForm />
     </div>
   );
